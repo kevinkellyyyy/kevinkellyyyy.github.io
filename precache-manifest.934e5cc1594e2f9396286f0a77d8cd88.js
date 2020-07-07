bb389c9a68694235d275bb16d77566faf3c12a08
kevinkellyyyy <kellyisyanta@gmail.com>
2020-07-07 22:38:42 +0700

    Updates

---
+++ precache-manifest.934e5cc1594e2f9396286f0a77d8cd88.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c44f3282ed3f845d3dc14730e43dea85",
+    "url": "/kevinisyanta/index.html"
+  },
+  {
+    "revision": "78482c9d6d556a4af239",
+    "url": "/kevinisyanta/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "b97743b1076a4f90e2bf",
+    "url": "/kevinisyanta/static/js/2.8f18329a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/kevinisyanta/static/js/2.8f18329a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "78482c9d6d556a4af239",
+    "url": "/kevinisyanta/static/js/main.2417ff88.chunk.js"
+  },
+  {
+    "revision": "25416d73083a66b43435",
+    "url": "/kevinisyanta/static/js/runtime-main.ba9ef0ba.js"
+  }
+]);
